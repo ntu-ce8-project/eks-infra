06d96fefb5a1d125bd08cb944a40c269a9c269b7
Jesstern Rays <jsstrn@users.noreply.github.com>
2025-03-28 20:16:51 +0800

    #22 fix: use kube-system namespace

--- addons/metrics-server/init.sh
+++ addons/metrics-server/init.sh
@@ -3,6 +3,6 @@
 helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
 
 helm upgrade --install metrics-server metrics-server/metrics-server \
-  --namespace monitoring \
+  --namespace kube-system \
   --create-namespace \
   --values values.yaml
