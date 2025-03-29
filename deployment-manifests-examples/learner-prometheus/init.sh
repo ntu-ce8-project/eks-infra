437d5b1176fc33a38521471911af4d2577897cd7
Jesstern Rays <jsstrn@users.noreply.github.com>
2025-03-29 17:23:53 +0800

    chore: delete example manifests

--- deployment-manifests-examples/learner-prometheus/init.sh
+++
@@ -1,7 +0,0 @@
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-
-# 2. Install or upgrade prometheus
-helm upgrade --install learner-prom prometheus-community/prometheus --version 69.6.0 \
-  --create-namespace \
-  --namespace learner-prom \
-  --values prometheus.yaml
