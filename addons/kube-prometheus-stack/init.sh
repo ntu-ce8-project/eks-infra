b23618b909e8ae785ced00235186d4b164fded0b
Jesstern Rays <jsstrn@users.noreply.github.com>
2025-03-30 13:47:32 +0800

    chore: add note for prometheus changes
    If we decide to use Grafana Cloud we may need to use a standalone Prometheus chart.
    
    See #26 and #30 for context.

--- addons/kube-prometheus-stack/init.sh
+++ addons/kube-prometheus-stack/init.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
-# 1. Add Helm repositories
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 
-# Update repos to ensure we have the latest charts
 helm repo update
 
-# 2. Install or upgrade prometheus
+# We may need to install a standalone prometheus chart first with a different values.yaml
+
+# helm upgrade --install prometheus prometheus-community/prometheus \
+#   --atomic \
+#   --version 27.7.1 \
+#   --create-namespace \
+#   --namespace monitoring \
+#   --values values.yaml
+
 helm upgrade --install kube-prometheus-stack prometheus-community/kube-prometheus-stack \
   --atomic \
   --version 69.6.0 \
   --create-namespace \
   --namespace monitoring \
   --values values.yaml
-
-
