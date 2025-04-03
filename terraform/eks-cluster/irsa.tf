fcb64de6f48f70790c2c298d738180c6580e4a4f
jason-leong-ihpc <jason-leong@ihpc.a-star.edu.sg>
2025-04-03 13:29:19 +0800

    test: karpenter policies

--- terraform/eks-cluster/irsa.tf
+++ terraform/eks-cluster/irsa.tf
@@ -143,32 +143,32 @@ module "ebs_csi_driver_role" {
 #   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:karpenter"]
 # }
 
-# resource "aws_iam_role" "karpenter_node_role" {
-#   name = "${local.prefix}-KarpenterNodeRole"
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "ec2.amazonaws.com"
-#         }
-#         Action = "sts:AssumeRole"
-#       }
-#     ]
-#   })
-# }
-# resource "aws_iam_role_policy_attachment" "karpenter_node_policy_attachment" {
-#   for_each = toset([
-#     "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
-#     "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
-#     "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
-#     "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-#   ])
+resource "aws_iam_role" "karpenter_node_role" {
+  name = "${local.prefix}-KarpenterNodeRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+resource "aws_iam_role_policy_attachment" "karpenter_node_policy_attachment" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  ])
 
-#   role       = aws_iam_role.karpenter_node_role.name
-#   policy_arn = each.value
-# }
+  role       = aws_iam_role.karpenter_node_role.name
+  policy_arn = each.value
+}
 
 # resource "aws_iam_policy" "karpenter_controller_policy" {
 #   name        = "${local.prefix}-KarpenterControllerPolicy"
