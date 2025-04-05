74e3e3eff96693692f0c65904b24697f4c84c91d
Jesstern Rays <jsstrn@users.noreply.github.com>
2025-04-06 01:17:46 +0800

    task: add distribution and use different url

--- load-testing/k6/test-prod.js
+++ load-testing/k6/test-prod.js
@@ -3,11 +3,14 @@ import { sleep } from 'k6';
 
 export const options = {
   cloud: {
-    name: 'Load test (prod)',
-  }
+    name: "Load test (prod)",
+    distribution: {
+      singapore: { loadZone: "amazon:sg:singapore", percent: 100 },
+    },
+  },
 };
 
 export default function() {
-  http.get('https://quickpizza.grafana.com');
+  http.get("https://postman-echo.com/get?hello=world");
   sleep(1);
 }
