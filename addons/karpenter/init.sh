b50fe2b07b62ea61f8f295bebb82ab4a8586faa5
jason-leong-ihpc <jason-leong@ihpc.a-star.edu.sg>
2025-04-05 15:50:46 +0800

    test: install karpenter by helm, add nodepool and ec2nodeclass

--- addons/karpenter/init.sh
+++ addons/karpenter/init.sh
@@ -8,14 +8,12 @@ echo "karpenter node role name: ${KARPENTER_NODE_ROLE_NAME}"
 echo "karpenter node instance profile name: ${KARPENTER_NODE_INSTANCE_PROFILE_NAME}"
 echo "karpenter sqs queue: ${KARPENTER_SQS_QUEUE_NAME}"
 
-
 export KARPENTER_NAMESPACE="kube-system"
 export KARPENTER_VERSION="1.3.3"
 
 helm repo add karpenter https://charts.karpenter.sh
 
 helm repo update
-
 
 # ### FOR INITIAL INSTALLATION ONLY ###
 # helm template karpenter oci://public.ecr.aws/karpenter/karpenter --version "${KARPENTER_VERSION}" --namespace "${KARPENTER_NAMESPACE}" \
@@ -28,16 +26,18 @@ helm repo update
 #     --set controller.resources.limits.cpu=1 \
 #     --set controller.resources.limits.memory=1Gi > karpenter.yaml
 
-
-# helm upgrade --install karpenter oci://public.ecr.aws/karpenter/karpenter \
-#   --version "${KARPENTER_VERSION}" \
-#   --namespace "karpenter" --create-namespace \
-#   --set "settings.clusterName=${EKS_CLUSTER_NAME}" \
-#   --set "settings.interruptionQueue=${KARPENTER_SQS_QUEUE}" \
-#   --set controller.resources.requests.cpu=1 \
-#   --set controller.resources.requests.memory=1Gi \
-#   --set controller.resources.limits.cpu=1 \
-#   --set controller.resources.limits.memory=1Gi \
-#   --set replicas=1 \
-#   --wait
-
+# helm registry logout public.ecr.aws
+helm upgrade --install karpenter oci://public.ecr.aws/karpenter/karpenter --version "${KARPENTER_VERSION}" \
+  --namespace "${KARPENTER_NAMESPACE}" --create-namespace \
+  --set serviceAccount.annotations."eks\.amazonaws\.com/role-arn"=${KARPENTER_CONTROLLER_ROLE_ARN} \
+  --set settings.clusterName=${CLUSTER_NAME} \
+  --set settings.clusterEndpoint=${CLUSTER_ENDPOINT} \
+  --set settings.featureGates.spotToSpotConsolidation=true \
+  --set settings.interruptionQueue=${CLUSTER_NAME} \
+  --set controller.resources.requests.cpu=1 \
+  --set controller.resources.requests.memory=1Gi \
+  --set controller.resources.limits.cpu=1 \
+  --set controller.resources.limits.memory=1Gi \
+  --wait
+  
+# helm uninstall karpenter --namespace "${KARPENTER_NAMESPACE}"
