10711ba995a62d4594518a2e2f537405a8584079
Jesstern Rays <jsstrn@users.noreply.github.com>
2025-04-06 00:46:22 +0800

    task: add load zone distribution

--- load-testing/k6/test-staging.js
+++ load-testing/k6/test-staging.js
@@ -1,13 +1,17 @@
-import http from 'k6/http';
-import { sleep } from 'k6';
+import http from "k6/http";
+import { sleep } from "k6";
 
 export const options = {
   cloud: {
-    name: 'Load test (staging)',
-  }
+    name: "Load test (staging)",
+    distribution: {
+      singapore: { loadZone: "amazon:sg:singapore", percent: 80 },
+      paris: { loadZone: "amazon:fr:paris", percent: 20 },
+    },
+  },
 };
 
-export default function() {
-  http.get('https://quickpizza.grafana.com');
+export default function () {
+  http.get("https://quickpizza.grafana.com");
   sleep(1);
 }
