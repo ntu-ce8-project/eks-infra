3bf93e6aed1522ae651e3882a3299e3d6d93ce52
jason-leong-ihpc <jason-leong@ihpc.a-star.edu.sg>
2025-04-06 08:34:26 +0800

    chore: cleanup

--- addons/karpenter/init.sh
+++ addons/karpenter/init.sh
@@ -37,7 +37,6 @@ helm upgrade --install karpenter oci://public.ecr.aws/karpenter/karpenter --vers
   --set controller.resources.requests.cpu=1 \
   --set controller.resources.requests.memory=1Gi \
   --set controller.resources.limits.cpu=1 \
-  --set controller.resources.limits.memory=1Gi \
-  --wait
+  --set controller.resources.limits.memory=1Gi
   
 # helm uninstall karpenter --namespace "${KARPENTER_NAMESPACE}"
